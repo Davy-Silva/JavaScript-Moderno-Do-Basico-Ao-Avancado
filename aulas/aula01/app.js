fe196750705c5ab868bdcd69a2905b7940f926d4
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-06-07 21:07:20 -0300

    Imprensão Js

--- aulas/aula01/app.js
+++ aulas/aula01/app.js
@@ -1,2 +1,1 @@
 console.log('Hello, Word!')
-window.alert('alerta')
---
+++ aulas/aula03/index.js
@@ -0,0 +1,15 @@
+console.log('Hello, Word!')
+
+// CERTO
+console.log('Davy "Silva"')
+console.log("Davy 'Silva'")
+
+// ERRADO
+//console.log('Davy 'Silva'')
+//console.log("Davy "Silva"")
+
+// CRAZE
+console.log(`'Davy' "Silva"`)
+
+// NUMBER
+console.log(15423, 423.43, 1.2, 53)
