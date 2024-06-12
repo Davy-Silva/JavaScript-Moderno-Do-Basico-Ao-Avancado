45a32177024e37dd56c241c0f98c2de29286c1f7
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-06-12 18:54:52 -0300

    Aula 4 e 5
    Comentários e Navegador

---
+++ aulas/aula04/comentarios.js
@@ -0,0 +1,19 @@
+// a escrever um comentário
+
+console.log('Olá Mundo!') // Aqui tem outro comentário
+console.log('Linha 1')
+// Comentário
+console.log('Linha 2')
+// console.log('Linha 3')
+
+console.log('Linha 1')
+/*
+console.log('Linha 2')
+console.log('Linha 3')
+*/
+console.log('Linha 4')
+
+
+/*
+Bloco de comentário
+*/
