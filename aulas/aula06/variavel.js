50293f8abac296b8a4e3c59454e4be4b70daf775
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-06-13 12:06:32 -0300

    Aula 6 e 7
    Variáveis e Constantes

---
+++ aulas/aula06/variavel.js
@@ -0,0 +1,14 @@
+let nome = 'João'
+console.log(nome)
+
+let nomeCliente = 'Alberto'
+/*
+    ERRO
+let nomeCliente
+*/
+console.log(nomeCliente)
+nomeCliente = 'maria'
+console.log(nomeCliente)
+
+console.log(nome, 'e', nomeCliente)
+
---
+++ aulas/aula07/constante.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5
+const segundoNumero = 10
+const conta = primeiroNumero * segundoNumero
+const contaDuplicada = conta * 2
+let resultadoTriplicado = conta * 3
+console.log('result =', conta)
+console.log('result x2 =', contaDuplicada)
+console.log('result x3 =', resultadoTriplicado)
+
+console.log(typeof(conta))
