512f5ad0a8b914838696d72c0bc30177e97a1a7a
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-06-18 09:31:21 -0300

    aula 11

--- aulas/aula11/operadoresaritmedicos.js
+++ aulas/aula11/operadoresaritmedicos.js
@@ -5,5 +5,14 @@ const num3 = 4
 console.log((num1 + num2) * num3)
 
 let contador = 1
+contador++
 ++contador
-console.log(contador)
+console.log(contador) 
+
+let conversao = parseInt('5')
+console.log(conversao)
+let conversao1 = parseFloat('5.2')
+console.log(conversao1)
+let conversao2 = Number('5')
+let conversao3 = Number('5.2')
+console.log(conversao2, conversao3)
