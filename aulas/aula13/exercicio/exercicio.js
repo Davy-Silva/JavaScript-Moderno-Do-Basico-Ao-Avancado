57c6b96f761e89cffbe72704393d0fe18dc38506
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-06-20 20:24:14 -0300

    Exercício De Strings Terminado

--- aulas/aula13/exercicio/exercicio.js
+++ aulas/aula13/exercicio/exercicio.js
@@ -1,15 +1,13 @@
+// VARIAVEIS
 const nome = prompt('Digite seu nome completo')
-const letras = nome.length
-const segundaLetra = nome[1]
 
-document.body.innerHTML = `O seu nome é ${nome}. <br/>`
-document.body.innerHTML += `Seu nome tem ${letras} letras. <br/>`
-document.body.innerHTML += `A segunda letra do seu nome é: ${segundaLetra}. <br/>`
-/*
-document.body.innerHTML += `Qual o primeiro índice da letra a no seu nome? ${}. <br/>`
-document.body.innerHTML += `Qual o último índice da letra a no seu nome? ${}. <br/>`
-document.body.innerHTML += `As últimas3 letras do seu noem são: ${}. <br/>`
-document.body.innerHTML += `As palavras do seu nome são: ${}. <br/>`
-document.body.innerHTML += `Seu nome com letras maiúsculas: ${}. <br/>`
-document.body.innerHTML += `Seu nome com letra minúsculas: ${}. <br/>`
-*/
+// EXIBIR NO NAVEGADOR
+document.body.innerHTML = `O seu nome é <strong>${nome}</strong>. <br/>`
+document.body.innerHTML += `Seu nome tem <strong>${nome.length}</strong> letras. <br/>`
+document.body.innerHTML += `A segunda letra do seu nome é: <strong>${nome[1].toUpperCase()}</strong>. <br/>`
+document.body.innerHTML += `Qual o primeiro índice da letra A no seu nome? <strong>${nome.indexOf('a')}</strong>. <br/>`
+document.body.innerHTML += `Qual o último índice da letra a no seu nome? <strong>${nome.lastIndexOf('a')}</strong>. <br/>`
+document.body.innerHTML += `As últimas 3 letras do seu noem são: <strong>${nome.slice(-3)}</strong>. <br/>`
+document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}</strong>. <br/>`
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong>. <br/>`
+document.body.innerHTML += `Seu nome com letra minúsculas: <strong>${nome.toLowerCase()}</strong>. <br/>`
