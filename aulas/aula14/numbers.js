1bdbe0ff841aeb4a0bcc65ab1723000eb07852c8
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-06-21 19:50:38 -0300

    Começo Aula 14
    Numbers

---
+++ aulas/aula14/numbers.js
@@ -0,0 +1,9 @@
+let num1 = 15
+let num2 = 2.5355
+
+console.log(num1.toString() + num2) // CONCATENANDO NUMBER
+console.log(typeof num1)
+console.log(num1.toString(2)) // REPRESENTAÇÃO BINÁRIA
+console.log(num2.toFixed(2)) // QUANTAS CASAS DECIMAIS MOSTRAR E ARREDONDAR
+
+// ALTERAR NUMBER PARA STING --> num1 = num1.toString()
