232b26a0a633f359a7d57b2ab3a69c9d4d8a4395
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-06-25 12:09:53 -0300

    Aula  14 e 15
    Numbers e Math

--- aulas/aula14/numbers.js
+++ aulas/aula14/numbers.js
@@ -11,13 +11,28 @@ console.log(num2.toFixed(2)) // QUANTAS CASAS DECIMAIS MOSTRAR E ARREDONDAR
 console.log(Number.isInteger(num1)) // RESPONDE SE É INTEIRO OU NÃO (BOOLEAN)
 
 let temp = num1 * 'ola'
-console.log(Number.isNaN(temp)) // RESPONDE SE A CONTA É FALSA OU NÃO (BOOLEAN)
+console.log(Number.isNaN(temp)) // RESPONDE SE A CONTA É VALIDA OU NÃO (BOOLEAN)
 
-let num3 = 0.7
-let num4 = 0.1
+num1 = 0.7
+num2 = 0.1
 
-num3 += num4
-num3 += num4
-num3 += num4
-num3 = num3.toFixed(2)
-console.log(num3)
+num1 += num2 // 0.8
+num1 += num2 // 0.9
+num1 += num2 // 1.0
+
+// NÃO RESOLVE, NUM3 NÃO VIRA UM NUMBER INTEIRO ->  num1 = num1.toFixed(2)
+num1 = Number(num1.toFixed(2)) // RESSOLVE, PASSA PARA INTEIRO E ARREDONDA
+// TAMBÉM RESOLVE -> num1 = parseFloat(num1.toFixed(2))
+
+console.log(num1)
+console.log(Number.isInteger(num1))
+
+// OUTRA SOLUÇÃO, MULTIPLICA POR 100 (TIRANDO AS CASAS DECIMAIS) E DEPOIS DIVIDE POR 100
+
+num1 = 0.7
+num2 = 0.1
+
+num1 = 100*(num1 + num2) / 100
+console.log(num1)
+
+// O JAVASCRIPT TEM IMPRECICÃO COM CALCÚLOS EM VALORES PEQUENOS, NAS CASAS DÉCIMAIS.
---
+++ aulas/aula15/math.js
@@ -0,0 +1,21 @@
+let num1 = 9.534534
+let num2 = Math.floor(num1) // Arredonda para baixo
+console.log(num2)
+
+num2 = Math.ceil(num1)
+console.log(num2) // Arrendonda para cima
+
+num2 = Math.round(num1) // Arredonda para o mais proximo
+console.log(num2)
+
+console.log(Math.max(1, 500, 0, 4, 15, 6, 47, 8, 9)) // Retorna o maior número dessa sequência
+console.log(Math.min(21, 34, 4, 7, 1, 84)) // Retorna o menor número dessa sequência
+
+console.log(Math.random()) // Gera um número aleatorio entre 0 e 1
+let aleatorio = Math.random() * (10 - 5) + 5 // Como fazer gerar um número aleatorio entre 10 e 5
+console.log(aleatorio)
+
+console.log(Math.PI) // Número de PI
+
+num1 = 9
+console.log(num1 ** (1/2)) // Calculo para descobrir a raiz quadrada
