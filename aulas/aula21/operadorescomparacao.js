7364f1d90ef5c065e747f0d64a9b22ff2744bd4e
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-07-05 09:46:41 -0300

    Início Aula 21
    - Logica De Programação
    - Operadores De Comparação

---
+++ aulas/aula21/operadorescomparacao.js
@@ -0,0 +1,32 @@
+/*
+    Operadores De Comparação
+
+    > Maior que
+    >= Maior ou igual que
+    < Menor que
+    <= Menor ou igual que
+
+    == Igualdade (valor)
+    === Igualdade estrita (valor e tipo)
+    != Diferente (valor)
+    !== Diferente estrito (valor e tipo)
+*/
+
+console.log(">:", 10 > 5) // Essas espressões sempre retornam valor boolean
+const comparacao = 5 > 5 // Pode criar variáveis com operadores
+console.log(">:", comparacao)
+
+console.log(">=:", 10 >= 5)
+console.log(">=:", 10 >= 5)
+
+console.log("<:", 10 < 5)
+
+console.log("<=:", 10 <= 5)
+
+console.log("==:", 10 == 5)
+
+console.log("===:", 10 === 5)
+
+console.log("!=:", 10 != 5)
+
+console.log("!==:", 10 !== 5)
