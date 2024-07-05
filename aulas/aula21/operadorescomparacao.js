8de2ee42a8bccae4e0dd4ab2a1b0ebbeeddf4e4d
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-07-05 14:17:38 -0300

    Aula 21 e 22
    - Operadores De Comparação
    - Operadores Lógicos

--- aulas/aula21/operadorescomparacao.js
+++ aulas/aula21/operadorescomparacao.js
@@ -1,32 +1,50 @@
-/*
-    Operadores De Comparação
-
-    > Maior que
-    >= Maior ou igual que
-    < Menor que
-    <= Menor ou igual que
-
-    == Igualdade (valor)
-    === Igualdade estrita (valor e tipo)
-    != Diferente (valor)
-    !== Diferente estrito (valor e tipo)
-*/
-
+/*    Operadores De Comparação
+ *
+ *    > Maior que
+ *    >= Maior ou igual
+ *    < Menor que
+ *    <= Menor ou igual
+ *
+ *    == Igualdade (só valor, não recomendado)
+ *    === Igualdade estrita (valor e tipo)
+ *    != Diferente (só valor, não recomendado)
+ *    !== Diferente estrito (valor e tipo)
+ */
 console.log(">:", 10 > 5) // Essas espressões sempre retornam valor boolean
 const comparacao = 5 > 5 // Pode criar variáveis com operadores
 console.log(">:", comparacao)
 
-console.log(">=:", 10 >= 5)
-console.log(">=:", 10 >= 5)
+console.log(" ")
 
+console.log(">=:", 10 >= 5)
+console.log(">=:", 4 >= 5)
+
+console.log(" ")
+
+console.log("<:", 5 < 10)
 console.log("<:", 10 < 5)
 
+console.log(" ")
+
+console.log("<=:", 5 <= 5)
 console.log("<=:", 10 <= 5)
 
+console.log(" ")
+
+console.log("==:", 10 == "10")
 console.log("==:", 10 == 5)
 
+console.log(" ")
+
+console.log("===:", 10 === 10)
 console.log("===:", 10 === 5)
 
+console.log(" ")
+
 console.log("!=:", 10 != 5)
+console.log("!=:", 10 != "10")
+
+console.log(" ")
 
 console.log("!==:", 10 !== 5)
+console.log("!==:", 10 !== 10)
---
+++ aulas/aula22/operadoreslogicos.js
@@ -0,0 +1,22 @@
+/*  
+ *  Operadores Lógicos
+ *
+ *  Trabalham com valores Booleans
+ * 
+ *  && / AND
+ *  || / OR
+ *  ! / NOT
+ */
+
+console.log(true && true) // Todas as expressões precisam ser verdadeiras para retornar true
+console.log(true && false)
+
+console.log(" ")
+
+console.log(true || false) // Pelo menos uma expressão precisar ser verdadeira para retornar true
+console.log(false || false)
+
+console.log(" ")
+
+console.log(!(false)) // Nega o resultado e retorna o resultado invertido
+console.log(!(true))
