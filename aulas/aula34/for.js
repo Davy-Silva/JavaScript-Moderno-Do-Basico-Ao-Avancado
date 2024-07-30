5f808385300a49ca120adc54964496953da933bf
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-07-30 20:44:51 -0300

    Update

---
+++ aulas/aula34/for.js
@@ -0,0 +1,13 @@
+/* Repetição
+console.log("Linha 1")
+console.log("Linha 2")
+console.log("Linha 3")
+console.log("Linha 4")
+console.log("Linha 5")
+*/
+
+// For é uma estrutura de repetição
+// i significa index
+for( let i = 1; i <= 5; i++ )
+{   console.log(`Linha ${i}`)   }
+
