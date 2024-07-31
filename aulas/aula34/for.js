1fb20fcc0e94aa07368cc07ea6a8049f8a6bf538
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-07-31 20:21:02 -0300

    Aula 34
    - For Clássico
    - Árvore DOM

--- aulas/aula34/for.js
+++ aulas/aula34/for.js
@@ -11,3 +11,18 @@ console.log("Linha 5")
 for( let i = 1; i <= 5; i++ )
 {   console.log(`Linha ${i}`)   }
 
+// Verificar se é par ou impar com for
+for(let a = 0; a <= 10; a++)
+{
+    const par = a % 2 === 0 ? "par" : "impar"
+    console.log(a, par)
+}
+
+// Exibindo todos os elementos de um Array
+const frutas = ["Maçã", "Pêra", "Uva"]
+
+for(let i = 0; i < frutas.length; i++)
+{
+    console.log(frutas[i])
+}
+
