280a79ef7e6aa89ce6c25617fd706e6bfd455bd4
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-09-12 12:44:44 -0300

    Exercício Da Aula 47 Finalizado
    - Timer

--- aulas/aula47/assets/js/main.js
+++ aulas/aula47/assets/js/main.js
@@ -1,11 +1,47 @@
-function criaHoraDosSegundos( segundos )
+function relogio()
 {
-    const data = new Date(segundos * 1000)
-    return data.toLocaleTimeString( 'pt-BR', { hour12: false, timeZone: 'GMT' } )
+    let timer
+    let segundos = 0
+    const relogio = document.querySelector('#relogio')
+    const buttonZerar = document.querySelector('#button-zerar')
+    const buttonPausar = document.querySelector('#button-pausar')
+    const buttonIniciar = document.querySelector('#button-iniciar')
+
+    function criaHoraDosSegundos( segundos )
+    {
+        const data = new Date(segundos * 1000)
+        return data.toLocaleTimeString( 'pt-BR', { hour12: false, timeZone: 'GMT' } )
+    }
+
+    function iniciaRelogio()
+    {
+        timer = setInterval( function()
+        {
+            segundos++
+            relogio.innerHTML = criaHoraDosSegundos( segundos )
+        }, 1000 )
+    }
+
+    console.log(criaHoraDosSegundos(10))
+
+    buttonIniciar.addEventListener( 'click', function( event )
+    {
+        clearInterval( timer )
+        relogio.classList.remove( 'pausado' )
+        iniciaRelogio()
+    })
+    buttonPausar.addEventListener( 'click', function( event )
+    {
+        clearInterval(timer)
+        relogio.classList.add( 'pausado' )
+    })
+    buttonZerar.addEventListener( 'click', function( event )
+    {
+        clearInterval(timer)
+        segundos = 0
+        relogio.classList.remove( 'pausado' )
+        relogio.innerHTML = '00:00:00'
+    })
 }
 
-console.log(criaHoraDosSegundos(10))
-
-const buttonZerar = document.querySelector('#button-zerar')
-const buttonPausar = document.querySelector('#button-pausar')
-const buttonIniciar = document.querySelector('#button-iniciar')
+relogio()
